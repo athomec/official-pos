63fe2bcf206af6b4b042de50275b27e586546a0f
vagabond <athomec@pie.com.tw>
2023-02-04 16:21:41 +0800

    上傳網站

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+$(function () {//JS開頭
+
+	var WINDOW = $(window).width();//視窗寬度
+	var WINDOWH = $(window).height();//視窗高度
+
+	//----------------gotop功能-----------------
+	$(".gotop").click(function () {
+		$("html,body").animate({ scrollTop: 0 }, 300);
+		return false;
+	})
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) { //若目前的位置距離網頁頂端>...
+			$(".gotop").addClass("show");
+			$(".js-nav").addClass("up");
+		} else {
+			$(".gotop").removeClass("show");
+			$(".js-nav").removeClass("up");
+		}
+	});
+	//banner 設定
+	RESIZE();
+
+	function RESIZE() {
+		var WINDOW = $(window).width();
+		var WINDOWH = $(window).height();
+
+		if (WINDOW < 768) {
+		}
+	}
+	$(window).resize(function () {
+		RESIZE();
+	})
+
+
+
+})//JS尾端	
+
